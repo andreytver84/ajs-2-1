41bf3e5e293b025d9a54ad1543dada8fb5060686
andreytver84 <uvikmedium@mail.ru>
2022-04-26 11:15:49 +0300

    first attempt

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCSSPxtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCSSPxtractPlugin.loader,
+                    'css-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+        new MiniCSSPxtractPlugin()
+    ]
+}
